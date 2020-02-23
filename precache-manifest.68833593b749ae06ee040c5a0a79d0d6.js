bad1f5e287ff08874b2fbddff0ddce4df7083915
qriscilla <pri.whang@gmail.com>
2020-02-23 10:35:20 -0500

    Updates

---
+++ precache-manifest.68833593b749ae06ee040c5a0a79d0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be9e9efc04149b7a92335acc7d13cb0",
+    "url": "/responsive-react/index.html"
+  },
+  {
+    "revision": "765de841f3af4fddcf68",
+    "url": "/responsive-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32c2dd52bc3268665ce6",
+    "url": "/responsive-react/static/js/2.860fca19.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/responsive-react/static/js/2.860fca19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765de841f3af4fddcf68",
+    "url": "/responsive-react/static/js/main.73389fee.chunk.js"
+  },
+  {
+    "revision": "0f19cf261230a0434865",
+    "url": "/responsive-react/static/js/runtime-main.502786a5.js"
+  }
+]);
